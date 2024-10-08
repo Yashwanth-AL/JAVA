688896df9d74f79bda8fdf35c7abe7533d24d2a3
Yashwanth A L <yashwanthal2004@gmail.com>
2024-10-08 16:53:50 +0530

    added code to use iterators in linked list

--- Practice/linkedList.java
+++ Practice/linkedList.java
@@ -1,9 +1,11 @@
 package Practice;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class linkedList {
-    record  Place(String name, int dist){
+
+    public record  Place(String name, int dist){
 
         @Override
         public String toString() {
@@ -11,8 +13,54 @@ public class linkedList {
                     ", dist = " + dist + " } ";
         }
     }
+
     public static void main(String[] args) {
         LinkedList<Place> list = new LinkedList<>();
+        testCase(list);
+
+        printMenu();
+        var iterator = list.listIterator();
+        boolean quitLoop = false;
+        Scanner sc = new Scanner(System.in);
+
+        while(!quitLoop){
+            System.out.println("Enter value ");
+            String option = sc.nextLine().toLowerCase().substring(0,1);
+
+            switch(option){
+                case "f" :
+                    if(iterator.hasNext()){
+                        System.out.println("Forward Movement " + iterator.next());
+                    } else {
+                        System.out.println("Cannot go forward since this is the ending point");
+                    }
+                    break;
+                case "b" :
+                    if(iterator.hasPrevious()){
+                        System.out.println("Backward Movement " + iterator.previous());
+                    }else {
+                        System.out.println("cannot go backward since this is the starting point");
+                    }
+                    break;
+                case "l" :
+                    for(var l : list){
+                        System.out.println(l);
+                    }
+                    break;
+                case "m" :
+                    printMenu();
+                    break;
+                case "q" :
+                    System.out.println("quiting");
+                    quitLoop = true;
+                    break;
+                default:
+                    quitLoop = true;
+            }
+        }
+    }
+
+    public static void testCase(LinkedList<Place> list){
         Place bangalore = new Place("Bangalore", 244);
         Place mysore = new Place("Mysore", 24);
         addPlace(list, bangalore);
@@ -24,6 +72,17 @@ public class linkedList {
         list.addFirst(new Place("kashmir", 0));
         list.addLast(new Place("kanyakumari", 23435));
         System.out.println(list);
+    }
+
+    public static void printMenu(){
+        System.out.println("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackwards
+                (L)ist Places
+                (M)enu
+                (Q)uit
+                """);
     }
 
     public static void addPlace(LinkedList<Place> list, Place place){
